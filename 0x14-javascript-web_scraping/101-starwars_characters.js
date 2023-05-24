91d7f0ba8f63de3dbff6d518786ddd9a887f99fb
FAITHOKOSUN <okosunfaith2@gmail.com>
2023-05-24 03:42:53 +0100

    js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,30 +1,28 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'http://swapi.co/api/films/';
-let id = parseInt(process.argv[2], 10);
-let characters = [];
 
-request(url, function (err, response, body) {
-  if (err == null) {
-    const resp = JSON.parse(body);
-    const results = resp.results;
-    if (id < 4) {
-      id += 3;
-    } else {
-      id -= 3;
-    }
-    for (let i = 0; i < results.length; i++) {
-      if (results[i].episode_id === id) {
-        characters = results[i].characters;
-        break;
-      }
-    }
-    for (let j = 0; j < characters.length; j++) {
-      request(characters[j], function (err, response, body) {
-        if (err == null) {
-          console.log(JSON.parse(body).name);
+const apiUrl = 'https://swapi-api.hbtn.io/api/films/';
+
+// Retrieve the movie ID from the command-line argument
+const movieId = process.argv[2];
+
+// Make a GET request to the Star Wars API for the movie details
+request(apiUrl + movieId, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const movie = JSON.parse(body);
+    const characters = movie.characters;
+
+    // Iterate over the characters array and print each character's name
+    characters.forEach((characterUrl) => {
+      request(characterUrl, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+          const character = JSON.parse(body);
+          console.log(character.name);
         }
       });
-    }
+    });
+  } else {
+    console.error('Error:', error);
   }
 });
+
